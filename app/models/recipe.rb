7029aff0f40cd3393daf422f6032c80591a6612b
PepperMcLean <jackmcl206@gmail.com>
2021-11-12 05:35:13 -0600

    added scope method most reviewed recipe to recipe index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,6 @@ class Recipe < ApplicationRecord
   validates :title, presence: true, length: {minimum: 4}
   validates :description, :ingredient_list, :instructions, presence: true, length: {minimum: 25}
   validates :required_appliances, presence: true, length: {minimum: 3}
+
+  scope :most_reviewed, -> {joins(:reviews).group('recipes.id').order('count(recipes.id) desc').limit(1)}
 end
